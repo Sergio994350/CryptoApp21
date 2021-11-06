c7146318906570e5314d4f763d634664998d1f7e
Sergey Tarasov <sila080907@gmail.com>
2021-11-06 17:26:12 +0300

    get data from internet

--- app/src/main/java/com/tarasov/cryptoapp21/MainActivity.kt
+++ app/src/main/java/com/tarasov/cryptoapp21/MainActivity.kt
@@ -2,11 +2,35 @@ package com.tarasov.cryptoapp21
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.tarasov.cryptoapp21.api.ApiFactory
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+
+    private val compositeDisposable = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//[2nd fun]//        val disposable = ApiFactory.apiService.getFullPriceList(fSyms = "BTC,ETH,EOS")
+        val disposable = ApiFactory.apiService.getTopCoinsInfo()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                       Log.d("TEST_OF_LOADING_DATA", it.toString())
+            },{
+                it.message?.let { it1 -> Log.d("TEST_OF_LOADING_DATA", it1) }
+            })
+        compositeDisposable.add(disposable)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
     }
 }
-//ls41
+//ls42
---
+++ app/src/main/java/com/tarasov/cryptoapp21/api/ApiFactory.kt
@@ -0,0 +1,18 @@
+package com.tarasov.cryptoapp21.api
+
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+object ApiFactory {
+
+    private const val BASE_URL = "https://min-api.cryptocompare.com/data/"
+
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .baseUrl(BASE_URL).build()
+
+    val apiService = retrofit.create(ApiService::class.java)
+}
